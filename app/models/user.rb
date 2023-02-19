dc7165aab87ab23032d104751ded187a5d37873a
Paulina Szarek <xpaula20@wp.pl>
2023-02-19 15:49:51 +0100

    do usuniecia

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :trackable
+  include DeviseTokenAuth::Concerns::User
   has_many :animals, dependent: :destroy
   has_secure_password
 
-  validates :first_name, presence: true, length: { minimum: 3, maximum: 20}
-  validates :last_name, presence: true, length: { minimum: 3, maximum: 20}
+  validates :first_name, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :last_name, presence: true, length: { minimum: 3, maximum: 20 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 105 }
 end
