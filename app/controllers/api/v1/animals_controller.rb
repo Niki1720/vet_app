bac93f27b8a64d48e802f98426774bb501fca56a
Paulina Szarek <xpaula20@wp.pl>
2023-03-12 12:36:15 +0100

    Add jwt gem

--- app/controllers/api/v1/animals_controller.rb
+++ app/controllers/api/v1/animals_controller.rb
@@ -1,6 +1,7 @@
 module Api
   module V1
     class AnimalsController < ApplicationController
+      before_action :authenticate_request
       before_action :set_user
       before_action :set_animal, only: [:show, :update, :destroy]
       # GET api/v1/animals
